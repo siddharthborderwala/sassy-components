81d7fd9807ba71496271cf106b3335bdd5f059c6
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-10-07 21:14:13 +0530

    initial commit

---
+++ dist/components.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const SlAlert = /*@__PURE__*/ createReactComponent('sl-alert');
+export const SlAnimation = /*@__PURE__*/ createReactComponent('sl-animation');
+export const SlAvatar = /*@__PURE__*/ createReactComponent('sl-avatar');
+export const SlBadge = /*@__PURE__*/ createReactComponent('sl-badge');
+export const SlButton = /*@__PURE__*/ createReactComponent('sl-button');
+export const SlButtonGroup = /*@__PURE__*/ createReactComponent('sl-button-group');
+export const SlCard = /*@__PURE__*/ createReactComponent('sl-card');
+export const SlCheckbox = /*@__PURE__*/ createReactComponent('sl-checkbox');
+export const SlColorPicker = /*@__PURE__*/ createReactComponent('sl-color-picker');
+export const SlDetails = /*@__PURE__*/ createReactComponent('sl-details');
+export const SlDialog = /*@__PURE__*/ createReactComponent('sl-dialog');
+export const SlDrawer = /*@__PURE__*/ createReactComponent('sl-drawer');
+export const SlDropdown = /*@__PURE__*/ createReactComponent('sl-dropdown');
+export const SlForm = /*@__PURE__*/ createReactComponent('sl-form');
+export const SlFormatBytes = /*@__PURE__*/ createReactComponent('sl-format-bytes');
+export const SlIcon = /*@__PURE__*/ createReactComponent('sl-icon');
+export const SlIconButton = /*@__PURE__*/ createReactComponent('sl-icon-button');
+export const SlImageComparer = /*@__PURE__*/ createReactComponent('sl-image-comparer');
+export const SlInput = /*@__PURE__*/ createReactComponent('sl-input');
+export const SlMenu = /*@__PURE__*/ createReactComponent('sl-menu');
+export const SlMenuDivider = /*@__PURE__*/ createReactComponent('sl-menu-divider');
+export const SlMenuItem = /*@__PURE__*/ createReactComponent('sl-menu-item');
+export const SlMenuLabel = /*@__PURE__*/ createReactComponent('sl-menu-label');
+export const SlProgressBar = /*@__PURE__*/ createReactComponent('sl-progress-bar');
+export const SlProgressRing = /*@__PURE__*/ createReactComponent('sl-progress-ring');
+export const SlRadio = /*@__PURE__*/ createReactComponent('sl-radio');
+export const SlRange = /*@__PURE__*/ createReactComponent('sl-range');
+export const SlRating = /*@__PURE__*/ createReactComponent('sl-rating');
+export const SlResponsiveEmbed = /*@__PURE__*/ createReactComponent('sl-responsive-embed');
+export const SlSelect = /*@__PURE__*/ createReactComponent('sl-select');
+export const SlSkeleton = /*@__PURE__*/ createReactComponent('sl-skeleton');
+export const SlSpinner = /*@__PURE__*/ createReactComponent('sl-spinner');
+export const SlSwitch = /*@__PURE__*/ createReactComponent('sl-switch');
+export const SlTab = /*@__PURE__*/ createReactComponent('sl-tab');
+export const SlTabGroup = /*@__PURE__*/ createReactComponent('sl-tab-group');
+export const SlTabPanel = /*@__PURE__*/ createReactComponent('sl-tab-panel');
+export const SlTag = /*@__PURE__*/ createReactComponent('sl-tag');
+export const SlTextarea = /*@__PURE__*/ createReactComponent('sl-textarea');
+export const SlTooltip = /*@__PURE__*/ createReactComponent('sl-tooltip');
+//# sourceMappingURL=components.js.map
---
+++ src/components.ts
@@ -0,0 +1,163 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@shoelace-style/shoelace';
+
+export const SlAlert = /*@__PURE__*/ createReactComponent<
+  JSX.SlAlert,
+  HTMLSlAlertElement
+>('sl-alert');
+export const SlAnimation = /*@__PURE__*/ createReactComponent<
+  JSX.SlAnimation,
+  HTMLSlAnimationElement
+>('sl-animation');
+export const SlAvatar = /*@__PURE__*/ createReactComponent<
+  JSX.SlAvatar,
+  HTMLSlAvatarElement
+>('sl-avatar');
+export const SlBadge = /*@__PURE__*/ createReactComponent<
+  JSX.SlBadge,
+  HTMLSlBadgeElement
+>('sl-badge');
+export const SlButton = /*@__PURE__*/ createReactComponent<
+  JSX.SlButton,
+  HTMLSlButtonElement
+>('sl-button');
+export const SlButtonGroup = /*@__PURE__*/ createReactComponent<
+  JSX.SlButtonGroup,
+  HTMLSlButtonGroupElement
+>('sl-button-group');
+export const SlCard = /*@__PURE__*/ createReactComponent<
+  JSX.SlCard,
+  HTMLSlCardElement
+>('sl-card');
+export const SlCheckbox = /*@__PURE__*/ createReactComponent<
+  JSX.SlCheckbox,
+  HTMLSlCheckboxElement
+>('sl-checkbox');
+export const SlColorPicker = /*@__PURE__*/ createReactComponent<
+  JSX.SlColorPicker,
+  HTMLSlColorPickerElement
+>('sl-color-picker');
+export const SlDetails = /*@__PURE__*/ createReactComponent<
+  JSX.SlDetails,
+  HTMLSlDetailsElement
+>('sl-details');
+export const SlDialog = /*@__PURE__*/ createReactComponent<
+  JSX.SlDialog,
+  HTMLSlDialogElement
+>('sl-dialog');
+export const SlDrawer = /*@__PURE__*/ createReactComponent<
+  JSX.SlDrawer,
+  HTMLSlDrawerElement
+>('sl-drawer');
+export const SlDropdown = /*@__PURE__*/ createReactComponent<
+  JSX.SlDropdown,
+  HTMLSlDropdownElement
+>('sl-dropdown');
+export const SlForm = /*@__PURE__*/ createReactComponent<
+  JSX.SlForm,
+  HTMLSlFormElement
+>('sl-form');
+export const SlFormatBytes = /*@__PURE__*/ createReactComponent<
+  JSX.SlFormatBytes,
+  HTMLSlFormatBytesElement
+>('sl-format-bytes');
+export const SlIcon = /*@__PURE__*/ createReactComponent<
+  JSX.SlIcon,
+  HTMLSlIconElement
+>('sl-icon');
+export const SlIconButton = /*@__PURE__*/ createReactComponent<
+  JSX.SlIconButton,
+  HTMLSlIconButtonElement
+>('sl-icon-button');
+export const SlImageComparer = /*@__PURE__*/ createReactComponent<
+  JSX.SlImageComparer,
+  HTMLSlImageComparerElement
+>('sl-image-comparer');
+export const SlInput = /*@__PURE__*/ createReactComponent<
+  JSX.SlInput,
+  HTMLSlInputElement
+>('sl-input');
+export const SlMenu = /*@__PURE__*/ createReactComponent<
+  JSX.SlMenu,
+  HTMLSlMenuElement
+>('sl-menu');
+export const SlMenuDivider = /*@__PURE__*/ createReactComponent<
+  JSX.SlMenuDivider,
+  HTMLSlMenuDividerElement
+>('sl-menu-divider');
+export const SlMenuItem = /*@__PURE__*/ createReactComponent<
+  JSX.SlMenuItem,
+  HTMLSlMenuItemElement
+>('sl-menu-item');
+export const SlMenuLabel = /*@__PURE__*/ createReactComponent<
+  JSX.SlMenuLabel,
+  HTMLSlMenuLabelElement
+>('sl-menu-label');
+export const SlProgressBar = /*@__PURE__*/ createReactComponent<
+  JSX.SlProgressBar,
+  HTMLSlProgressBarElement
+>('sl-progress-bar');
+export const SlProgressRing = /*@__PURE__*/ createReactComponent<
+  JSX.SlProgressRing,
+  HTMLSlProgressRingElement
+>('sl-progress-ring');
+export const SlRadio = /*@__PURE__*/ createReactComponent<
+  JSX.SlRadio,
+  HTMLSlRadioElement
+>('sl-radio');
+export const SlRange = /*@__PURE__*/ createReactComponent<
+  JSX.SlRange,
+  HTMLSlRangeElement
+>('sl-range');
+export const SlRating = /*@__PURE__*/ createReactComponent<
+  JSX.SlRating,
+  HTMLSlRatingElement
+>('sl-rating');
+export const SlResponsiveEmbed = /*@__PURE__*/ createReactComponent<
+  JSX.SlResponsiveEmbed,
+  HTMLSlResponsiveEmbedElement
+>('sl-responsive-embed');
+export const SlSelect = /*@__PURE__*/ createReactComponent<
+  JSX.SlSelect,
+  HTMLSlSelectElement
+>('sl-select');
+export const SlSkeleton = /*@__PURE__*/ createReactComponent<
+  JSX.SlSkeleton,
+  HTMLSlSkeletonElement
+>('sl-skeleton');
+export const SlSpinner = /*@__PURE__*/ createReactComponent<
+  JSX.SlSpinner,
+  HTMLSlSpinnerElement
+>('sl-spinner');
+export const SlSwitch = /*@__PURE__*/ createReactComponent<
+  JSX.SlSwitch,
+  HTMLSlSwitchElement
+>('sl-switch');
+export const SlTab = /*@__PURE__*/ createReactComponent<
+  JSX.SlTab,
+  HTMLSlTabElement
+>('sl-tab');
+export const SlTabGroup = /*@__PURE__*/ createReactComponent<
+  JSX.SlTabGroup,
+  HTMLSlTabGroupElement
+>('sl-tab-group');
+export const SlTabPanel = /*@__PURE__*/ createReactComponent<
+  JSX.SlTabPanel,
+  HTMLSlTabPanelElement
+>('sl-tab-panel');
+export const SlTag = /*@__PURE__*/ createReactComponent<
+  JSX.SlTag,
+  HTMLSlTagElement
+>('sl-tag');
+export const SlTextarea = /*@__PURE__*/ createReactComponent<
+  JSX.SlTextarea,
+  HTMLSlTextareaElement
+>('sl-textarea');
+export const SlTooltip = /*@__PURE__*/ createReactComponent<
+  JSX.SlTooltip,
+  HTMLSlTooltipElement
+>('sl-tooltip');
