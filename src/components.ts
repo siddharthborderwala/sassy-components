0a382af248df6a67ee8f68898f23746f1fe8555b
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-10-08 07:26:36 +0530

    export styles and defining custom elements

--- src/components.ts
+++ src/components.ts
@@ -3,7 +3,7 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import type { JSX } from '@shoelace-style/shoelace';
+import { JSX } from '@shoelace-style/shoelace';
 
 export const SlAlert = /*@__PURE__*/ createReactComponent<
   JSX.SlAlert,
