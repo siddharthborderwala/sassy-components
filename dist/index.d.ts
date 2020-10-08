82d07acf2a35fa2b6c29b5778444eb6b9a188025
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-10-08 07:28:58 +0530

    version 0.1.3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,5 @@
+import * as styles from '@shoelace-style/shoelace/dist/shoelace/shoelace.css';
+import * as darkStyles from '@shoelace-style/shoelace/themes/dark.css';
+export { styles, darkStyles };
 export * from './components';
+export * from '@shoelace-style/shoelace/loader';
